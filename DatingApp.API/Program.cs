f48ae8616488e6673547d2916c2f54f8e09a2d39
xinghaoru <867791585@qq.com>
2020-03-22 03:44:11 -0400

    add UserController

--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -25,9 +25,11 @@ namespace DatingApp.API
                 catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occured during migtstions");
+                    logger.LogError(ex, "An error occured during migration");
                 }
             }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
